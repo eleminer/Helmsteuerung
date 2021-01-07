e04621f312dd856bbf3595fa4ea54d409629c224
Marc <pansmarcjoe@t-online.de>
2021-01-07 18:28:15 +0100

    added GPIO_APP_Speech Python Programm

--- PluginSopare/__init__.py
+++ PluginSopare/__init__.py
@@ -1,15 +1,16 @@
-from flask import Flask
-from flask import request
-import RPi.GPIO as GPIO
-from picamera import PiCamera
-from time import sleep
-from concurrent.futures import ThreadPoolExecutordef run(readable_results, data, rawbuf):
-    print readable_results
+def run(readable_results, data, rawbuf):
+    f = open("/home/pi/Downloads/arguments.txt", "w")
     if "live" in readable_results: 
         print("Livebild angeschaltet")
+        f.write("live")
     if "aus" in readable_results: 
         print("Livebild ausgeschaltet")
+        f.write("aus")
     if "auf" in readable_results: 
         print("Servo auf")
+        f.write("auf")
     if "zu" in readable_results: 
         print("Servo zu")
+        f.write("zu")
+    f.close()
+
